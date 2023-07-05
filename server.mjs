630cba7af92594b5c420dc59714c283b4e2da1a1
David <zurek.dav@gmail.com>
2023-07-06 00:11:07 +0200

    Implement retrieving data for publishers and authors from backend

--- server.mjs
+++ server.mjs
@@ -1,5 +1,6 @@
 import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
+import axios from "axios"
 
 // The GraphQL schema
 const typeDefs = `#graphql
@@ -8,11 +9,13 @@ const typeDefs = `#graphql
     allAuthors: [Author]
   }
   type Publisher {
+    uuid: String!
     name: String
     description: String
     authors: [Author]
   }
   type Author {
+    uuid: String!
     firstName: String
     lastName: String
   }
@@ -21,17 +24,37 @@ const typeDefs = `#graphql
 // A map of functions which return data for the schema.
 const resolvers = {
 	Query: {
-		allPublishers: () => {
-			return [
-				{
-					name: "StandardEbooks",
-					description: "Hello World"
+		allPublishers: async () => {
+			try {
+				let response = await axios({
+					method: "get",
+					url: "http://localhost:3111/v2/table_objects",
+					params: {
+						table_id: 33
+					}
+				})
+
+				let result = []
+
+				for (let obj of response.data.table_objects) {
+					result.push({
+						uuid: obj.uuid,
+						name: obj.properties.name,
+						description: obj.properties.description,
+						authors: obj.properties.authors
+					})
 				}
-			]
+
+				return result
+			} catch (error) {
+				console.error(error.response.data)
+				return []
+			}
 		},
 		allAuthors: () => {
 			return [
 				{
+					uuid: "weqweqweqwe",
 					firstName: "Lemony",
 					lastName: "Snicket"
 				}
@@ -39,13 +62,32 @@ const resolvers = {
 		}
 	},
 	Publisher: {
-		authors: () => {
-			return [
-				{
-					firstName: "George",
-					lastName: "Orwell"
+		authors: async publisher => {
+			if (publisher.authors == null) {
+				return []
+			}
+
+			let authorUuids = publisher.authors.split(",")
+			let authors = []
+
+			for (let uuid of authorUuids) {
+				try {
+					let response = await axios({
+						method: "get",
+						url: `http://localhost:3111/v2/table_objects/${uuid}`
+					})
+
+					authors.push({
+						uuid: response.data.uuid,
+						firstName: response.data.properties.first_name,
+						lastName: response.data.properties.last_name
+					})
+				} catch (error) {
+					console.error(error.response.data)
 				}
-			]
+			}
+
+			return authors
 		}
 	}
 }
