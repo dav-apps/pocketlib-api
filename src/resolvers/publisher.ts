03a8da956854ddec5412d04fbb6ee5ee640f8f2d
David <zurek.dav@gmail.com>
2023-07-22 07:19:29 +0200

    Add the ability to retrieve the publisher of the user with uuid = 'mine'

--- src/resolvers/publisher.ts
+++ src/resolvers/publisher.ts
@@ -1,20 +1,56 @@
-import { List, Publisher, PublisherLogo, Author } from "../types.js"
+import {
+	List,
+	User,
+	TableObject,
+	Publisher,
+	PublisherLogo,
+	Author
+} from "../types.js"
 import {
 	convertTableObjectToPublisher,
 	convertTableObjectToPublisherLogo,
 	convertTableObjectToAuthor
 } from "../utils.js"
+import { admins } from "../constants.js"
 import { getTableObject, listTableObjects } from "../services/apiService.js"
 
 export async function retrievePublisher(
 	parent: any,
-	args: { uuid: string }
+	args: { uuid: string },
+	context: any
 ): Promise<Publisher> {
 	const uuid = args.uuid
 	if (uuid == null) return null
 
-	let tableObject = await getTableObject(uuid)
-	if (tableObject == null) return null
+	let tableObject: TableObject = null
+
+	if (uuid == "mine") {
+		// Check if the user is a publisher
+		const user: User = context.user
+
+		if (user == null) {
+			throw new Error("You are not authenticated")
+		} else if (admins.includes(user.id)) {
+			throw new Error("You are an admin")
+		}
+
+		// Get the publisher of the user
+		let response = await listTableObjects({
+			caching: false,
+			limit: 1,
+			tableName: "Publisher",
+			userId: user.id
+		})
+
+		if (response.items.length == 1) {
+			tableObject = response.items[0]
+		} else {
+			return null
+		}
+	} else {
+		tableObject = await getTableObject(uuid)
+		if (tableObject == null) return null
+	}
 
 	return convertTableObjectToPublisher(tableObject)
 }
