45fd32df95144fbaa4eab62d941154b6d4a5bd33
David <zurek.dav@gmail.com>
2024-11-16 21:32:53 +0100

    Update string to slug function with support for ß

--- src/utils.ts
+++ src/utils.ts
@@ -333,11 +333,13 @@ export function stringToSlug(str: string): string {
 	// remove accents, swap ñ for n, etc
 	var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;"
 	var to = "aaaaeeeeiiiioooouuuunc------"
+
 	for (var i = 0, l = from.length; i < l; i++) {
 		str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i))
 	}
 
 	return str
+		.replaceAll("ß", "ss") // replace ß with ss
 		.replace(/[^a-z0-9 -]/g, "") // remove invalid chars
 		.replace(/\s+/g, "-") // collapse whitespace and replace by -
 		.replace(/-+/g, "-") // collapse dashes
